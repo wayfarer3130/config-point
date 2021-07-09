032fb81ed546f49f7b144bc4f03a486a552a9337
Bill Wallace <wayfarer3130@gmail.com>
2021-07-09 16:24:07 -0400

    Initial commit, still working on it.

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+// https://github.com/facebook/jest/issues/3613
+// Yarn Doctor: `npx @yarnpkg/doctor .` -->
+// '<rootDir>' warning:
+// Strings should avoid referencing the node_modules directory (prefer require.resolve)
+
+module.exports = {
+  verbose: true,
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.jsx$': 'babel-jest'
+  },
+  testMatch: ['<rootDir>/src/**/*.test.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  // Setup
+  // setupFiles: ["jest-canvas-mock/lib/index.js"],
+  // Coverage
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx}',
+    // Not
+    '!<rootDir>/src/**/*.test.js',
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+    '!<rootDir>/dist/**',
+  ],
+};
